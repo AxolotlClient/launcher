f3ca9ea58c42d55e60675ba3d380567b4ea8262e
moehreag <moehreag@gmail.com>
2022-08-01 19:10:55 +0200

    semi-functional settings button (Texture missing)

--- js/app.mjs
+++ js/app.mjs
@@ -2,12 +2,37 @@ import Launcher from "./launcher/launcher.mjs";
 
 const playButton = document.querySelector(".play");
 
+const launcher = new Launcher();
+
 playButton.onclick = async() => {
     try {
-        let l = new Launcher(); 
-        await l.launch({ "version": "1.8.9" });
+        await launcher.launch({ "version": "1.8.9" });
     } catch (error) {
         // TODO better error handling
         console.error(error);
     }
 };
+
+const settingsButton = document.querySelector(".open_settings");
+const main = document.querySelector(".main");
+const settings = document.querySelector(".settings");
+
+settingsButton.onclick = () => {
+    try {
+        main.style.visibility = "hidden";
+        settings.style.visibility = "visible";
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const exitButton = document.querySelector(".close_settings");
+
+exitButton.onclick = () => {
+    try {
+        main.style.visibility = "visible";
+        settings.style.visibility = "hidden";
+    } catch (error) {
+        console.error(error);
+    }
+}
