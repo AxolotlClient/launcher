4abb06e5a9f6673cd30531156b4f116da65836ae
TheKodeToad <TheKodeToad@proton.me>
2022-09-18 18:04:39 +0100

    Instance switcher with eagerly-flipping triangle

--- js/app.mjs
+++ js/app.mjs
@@ -3,13 +3,25 @@ import { invoke } from "@tauri-apps/api";
 const playButton = document.querySelector(".play");
 
 playButton.onclick = async() => {
-    invoke('launch')
+    invoke("launch");
 };
+
+const instance = document.querySelector(".instance");
+const currentInstanceButton = document.querySelector(".current_instance");
+let instanceMenuOpen = false;
+
+instance.querySelectorAll("button").forEach((button) => button.onclick = () => {
+    instance.classList.toggle("extended");
+    instanceMenuOpen = !instanceMenuOpen;
+    currentInstanceButton.querySelector("img").style.transform = instanceMenuOpen ? "scaleY(-1)" : "scaleY(1)";
+    const old = currentInstanceButton.innerText;
+    currentInstanceButton.querySelector("p").innerText = button.innerText;
+    button.querySelector("p").innerText = old;
+});
 
 const settingsButton = document.querySelector(".open_settings");
 const main = document.querySelector(".main");
 const settings = document.querySelector(".settings");
-const bg = document.querySelector(".bg_image")
 
 settingsButton.onclick = () => {
     main.style.display = "none";
